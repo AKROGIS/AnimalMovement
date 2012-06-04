d39fa8f1e434eebcc6e687916a97d313308d7708
Regan Sarwas <regan_sarwas@nps.gov>
2012-06-04 10:15:45 -0800

    Changed settings files to use the regional DB
    Note that the name of the connection string in the datamodel class
    library is different, and this is the one that must be used in the
    app.config file.  the one created for with the app is no good, because
    it uses the wrong namespace.

--- AnimalMovement/Properties/Settings.Designer.cs
+++ AnimalMovement/Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace AnimalMovement.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=INPAKRO39088\\SQL2008R2;Initial Catalog=Animal_Movement;Integrated Sec" +
-            "urity=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=INPAKROMS53AIS;Initial Catalog=Animal_Movement;Integrated Security=Tr" +
+            "ue")]
         public string Animal_MovementConnectionString {
             get {
                 return ((string)(this["Animal_MovementConnectionString"]));
