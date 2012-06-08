ef3a4562d8e96e4deed600afd29bfc746539ab0f
Regan Sarwas <regan_sarwas@nps.gov>
2012-06-08 15:55:53 -0800

    changes to assembly/file versions

--- AnimalMovement/Properties/AssemblyInfo.cs
+++ AnimalMovement/Properties/AssemblyInfo.cs
@@ -63,5 +63,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Do not use the auto update of assembly versions, it cuases a reset of settings each time
+// and it isn't really visible.  Update the file version each time a new exe is published. 
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
