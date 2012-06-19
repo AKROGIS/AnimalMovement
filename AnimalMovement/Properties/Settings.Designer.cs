bcf5facb3cee0a1bb1c7377aea3bafc476523788
Regan Sarwas <regan_sarwas@nps.gov>
2012-06-19 10:00:01 -0800

    Minor cleanup to the app settings/config file configuration/
    Removed connection string setting in main project

--- AnimalMovement/Properties/Settings.Designer.cs
+++ AnimalMovement/Properties/Settings.Designer.cs
@@ -23,17 +23,6 @@ namespace AnimalMovement.Properties {
             }
         }
         
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=INPAKRO39088\\SQL2008R2;Initial Catalog=Animal_Movement;Integrated Sec" +
-            "urity=True")]
-        public string Animal_MovementConnectionString {
-            get {
-                return ((string)(this["Animal_MovementConnectionString"]));
-            }
-        }
-        
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
