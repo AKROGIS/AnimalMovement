2744bbff914fe867f581095a6844d9c1e0650bb7
Regan Sarwas (work) <regan_sarwas@nps.gov>
2012-07-30 13:05:04 -0800

    Add item to to do list

--- AnimalMovement/Properties/AssemblyInfo.cs
+++ AnimalMovement/Properties/AssemblyInfo.cs
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices;
 //TODO - DATABASE - Move collar deployment date checking from Stored procedure to trigger to protect against SA and to allow bulk uploading.
 //TODO - DATABASE - Add more unit testing.
 
+//FIXME - Telonics Gen 3 skip all (even good) records in a set if fix 1 is bad
 //TODO - Replace the wait cursors with a message box and progress bar
 //TODO - Consider viewer permission issues - All NPS is too permissive for some PIs
 //TODO - Support other file formats
