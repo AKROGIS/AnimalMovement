9f0da8fc64a6865ecb60d741accbe93381e14801
Regan Sarwas (work) <regan_sarwas@nps.gov>
2012-08-21 15:07:05 -0800

    Correct for Null dates in LocalTime
    Also add UtcTime to convert existing local times to Utc Time

--- SqlServerExtensions/AnimalMovementFunctions.cs
+++ SqlServerExtensions/AnimalMovementFunctions.cs
@@ -24,7 +24,17 @@ namespace SqlServerExtensions
         [SqlFunction]
         public static SqlDateTime LocalTime(SqlDateTime utcDateTime)
         {
+            if (utcDateTime.IsNull)
+                return SqlDateTime.Null;
             return new SqlDateTime(utcDateTime.Value.ToLocalTime());
+        }
+
+        [SqlFunction]
+        public static SqlDateTime UtcTime(SqlDateTime localDateTime)
+        {
+            if (localDateTime.IsNull)
+                return SqlDateTime.Null;
+            return new SqlDateTime(localDateTime.Value.ToUniversalTime());
         }
 
         //Code - Format
