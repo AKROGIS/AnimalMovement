bb9816a21aa885998cb5370c6670bb25a3bced45
Regan Sarwas (work) <regan_sarwas@nps.gov>
2012-10-11 09:24:46 -0800

    Fixed rounding error for TPF frequencies

--- TpfFilesSummerizer/Program.cs
+++ TpfFilesSummerizer/Program.cs
@@ -10,7 +10,7 @@ namespace TpfFilesSummerizer
             var collars = args.SelectMany(arg => TpfFile.LoadFromPath(arg).ParseForCollars());
             foreach (var collar in collars.OrderBy(c => c.ArgosId))
                 //Console.WriteLine(collar);
-                Console.WriteLine("{0},{1},{2:f2},{3}", collar.Ctn, collar.ArgosId, collar.Frequency, collar.TpfFile);
+                Console.WriteLine("{0},{1},{2:f3},{3}", collar.Ctn, collar.ArgosId, collar.Frequency, collar.TpfFile);
         }
     }
 }
