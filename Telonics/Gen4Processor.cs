bbaf2f5cdc40caefbe98ed07a557e7b42962c59f
Regan Sarwas @ Bob <rsarwas@gmail.com>
2013-02-28 21:14:17 -0900

    Put TDC file cleanup in finally block

--- Telonics/Gen4Processor.cs
+++ Telonics/Gen4Processor.cs
@@ -57,71 +57,92 @@ namespace Telonics
 
         private IEnumerable<string> ProcessFile(string fileContents)
         {
-            if (!File.Exists(TdcExecutable))
-                throw new InvalidOperationException("TDC Execution error - TDC not found at " + TdcExecutable);
+            string tpfPath = null;
+            string dataFilePath = null;
+            string batchFilePath = null;
+            string logFilePath = null;
+            string outputFolder = null;
+            string[] paths = null;
+            string[] results;
 
-            // save the tpf file to the file system
-            string tpfPath = Path.GetTempFileName();
-            File.WriteAllBytes(tpfPath, TpfFile);
-
-            // write the argos file transmission to the filesystem
-            var dataFilePath = Path.GetTempFileName();
-            //TDC batch mode uses the aws extension to determine file type 
-            if (fileContents.StartsWith("\"programNumber\";\"platformId\";"))
-                dataFilePath = dataFilePath + ".aws";
-            File.WriteAllText(dataFilePath, fileContents);
-
-            var outputFolder = GetNewTempDirectory();
-            var batchFilePath = Path.GetTempFileName();
-            var logFilePath = Path.GetTempFileName();
-
-            //Create the batch file and save it to the filesystem
-            string batchCommands = String.Format(BatchFileTemplate, dataFilePath, tpfPath, outputFolder, logFilePath);
-            File.WriteAllText(batchFilePath, batchCommands);
-
-            //  Run TDC with the batch file
-            var p = Process.Start(new ProcessStartInfo
+            try
             {
-                FileName = TdcExecutable,
-                Arguments = "/batch:\"" + batchFilePath +"\"",
-                CreateNoWindow = true,
-                UseShellExecute = false,
-                RedirectStandardError = true
-            });
-            string errors = p.StandardError.ReadToEnd();
-            p.WaitForExit();
+                if (!File.Exists(TdcExecutable))
+                    throw new InvalidOperationException("TDC Execution error - TDC not found at " + TdcExecutable);
+                
+                // save the tpf file to the file system
+                tpfPath = Path.GetTempFileName();
+                File.WriteAllBytes(tpfPath, TpfFile);
+                
+                // write the argos file transmission to the filesystem
+                dataFilePath = Path.GetTempFileName();
+                //TDC batch mode uses the aws extension to determine file type 
+                if (fileContents.StartsWith("\"programNumber\";\"platformId\";"))
+                    dataFilePath = dataFilePath + ".aws";
+                File.WriteAllText(dataFilePath, fileContents);
+                
+                batchFilePath = Path.GetTempFileName();
+                logFilePath = Path.GetTempFileName();
+                outputFolder = GetNewTempDirectory();
 
-            if (!String.IsNullOrEmpty(errors))
-                throw new InvalidOperationException("TDC Execution error " + errors);
+                //Create the batch file and save it to the filesystem
+                string batchCommands = String.Format(BatchFileTemplate, dataFilePath, tpfPath, outputFolder, logFilePath);
+                File.WriteAllText(batchFilePath, batchCommands);
+                
+                //  Run TDC with the batch file
+                //FIXME - put a timeout on this command
+                var p = Process.Start(new ProcessStartInfo
+                                      {
+                    FileName = TdcExecutable,
+                    Arguments = "/batch:\"" + batchFilePath +"\"",
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardError = true
+                });
+                string errors = p.StandardError.ReadToEnd();
+                p.WaitForExit();
+                
+                if (!String.IsNullOrEmpty(errors))
+                    throw new InvalidOperationException("TDC Execution error " + errors);
+                
+                //FIXME - check and report errors in the log file
+                //Here is an example log file with an error:
+                /*
+                Batch started at: 2012.12.17 22:04:31
+                Processing file: C:\Users\resarwas\AppData\Local\Temp\tmpB158.tmp
+                Unable to load the the parameter file: "C:\Users\resarwas\Documents\Visual Studio 2010\Projects\AnimalMovement\ArgosProcessor\bin\Debug\tpf\23.tpf".This file may require a newer version of TDC.
+                Batch completed at: 2012.12.17 22:32:27
+                */
+                
+                // for each output file created by TDC, send the file to the database
+                paths = Directory.GetFiles(outputFolder);
+                if (paths.Length < 1)
+                    throw new InvalidOperationException("TDC Execution error - No output file");
+                if (paths.Length > 1)
+                    throw new InvalidOperationException("TDC Execution error - multiple output files");
+                
+                results = File.ReadAllLines(paths[0]);
 
-            //FIXME - check and report errors in the log file
-            //Here is an example log file with an error:
-            /*
-            Batch started at: 2012.12.17 22:04:31
-            Processing file: C:\Users\resarwas\AppData\Local\Temp\tmpB158.tmp
-            Unable to load the the parameter file: "C:\Users\resarwas\Documents\Visual Studio 2010\Projects\AnimalMovement\ArgosProcessor\bin\Debug\tpf\23.tpf".This file may require a newer version of TDC.
-            Batch completed at: 2012.12.17 22:32:27
-            */
-
-            // for each output file created by TDC, send the file to the database
-            var paths = Directory.GetFiles(outputFolder);
-            if (paths.Length < 1)
-                throw new InvalidOperationException("TDC Execution error - No output file");
-            if (paths.Length > 1)
-                throw new InvalidOperationException("TDC Execution error - multiple output files");
-
-            var results = File.ReadAllLines(paths[0]);
-
-            //cleanup temp files/folders
-            //FIXME - put this in a finally block, so cleanup is always done.
-            //in a finally block, need to figure out what has been created and what has not, and need to protect against exceptions
-            File.SetAttributes(paths[0], FileAttributes.Normal);  // remove the readonly flag put on files created by TDC.
-            File.Delete(paths[0]);
-            Directory.Delete(outputFolder);
-            File.Delete(logFilePath);
-            File.Delete(batchFilePath);
-            File.Delete(dataFilePath);
-            File.Delete(tpfPath);
+            }
+            finally
+            {
+                //cleanup temp files/folders
+                if (tpfPath != null && File.Exists(tpfPath))
+                    File.Delete(tpfPath);
+                if (dataFilePath != null && File.Exists(dataFilePath))
+                    File.Delete(dataFilePath);
+                if (batchFilePath != null && File.Exists(batchFilePath))
+                    File.Delete(batchFilePath);
+                if (logFilePath != null && File.Exists(logFilePath))
+                    File.Delete(logFilePath);
+                if (paths[0] != null && File.Exists(paths[0]))
+                {
+                    File.SetAttributes(paths[0], FileAttributes.Normal);  // remove the readonly flag put on files created by TDC.
+                    File.Delete(paths[0]);
+                }
+                if (outputFolder != null && Directory.Exists(outputFolder))
+                    Directory.Delete(outputFolder);
+            }
 
             return results;
         }
