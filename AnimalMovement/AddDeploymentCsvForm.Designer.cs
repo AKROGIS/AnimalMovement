2dc89c7efa11825d00edb47dce720d6a3506652f
Regan Sarwas (work) <regan_sarwas@nps.gov>
2013-05-06 15:01:15 -0800

    Add CSV Deployment data form

---
+++ AnimalMovement/AddDeploymentCsvForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace AnimalMovement
+{
+    partial class AddDeploymentCsvForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.QuitButton = new System.Windows.Forms.Button();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.InvestigatorComboBox = new System.Windows.Forms.ComboBox();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FileNameTextBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Project Investigator:";
+            // 
+            // QuitButton
+            // 
+            this.QuitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.QuitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.QuitButton.Location = new System.Drawing.Point(93, 131);
+            this.QuitButton.Name = "QuitButton";
+            this.QuitButton.Size = new System.Drawing.Size(75, 23);
+            this.QuitButton.TabIndex = 6;
+            this.QuitButton.Text = "Cancel";
+            this.QuitButton.UseVisualStyleBackColor = true;
+            // 
+            // NextButton
+            // 
+            this.NextButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.NextButton.Location = new System.Drawing.Point(174, 131);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(75, 23);
+            this.NextButton.TabIndex = 5;
+            this.NextButton.Text = "Process";
+            this.NextButton.UseVisualStyleBackColor = true;
+            // 
+            // InvestigatorComboBox
+            // 
+            this.InvestigatorComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InvestigatorComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.InvestigatorComboBox.FormattingEnabled = true;
+            this.InvestigatorComboBox.Location = new System.Drawing.Point(121, 12);
+            this.InvestigatorComboBox.Name = "InvestigatorComboBox";
+            this.InvestigatorComboBox.Size = new System.Drawing.Size(128, 21);
+            this.InvestigatorComboBox.TabIndex = 4;
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BrowseButton.Location = new System.Drawing.Point(223, 39);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(24, 23);
+            this.BrowseButton.TabIndex = 29;
+            this.BrowseButton.Text = "...";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "File:";
+            // 
+            // FileNameTextBox
+            // 
+            this.FileNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FileNameTextBox.Location = new System.Drawing.Point(50, 41);
+            this.FileNameTextBox.Name = "FileNameTextBox";
+            this.FileNameTextBox.Size = new System.Drawing.Size(167, 20);
+            this.FileNameTextBox.TabIndex = 28;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(11, 68);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 57);
+            this.textBox1.TabIndex = 31;
+            // 
+            // AddDeploymentCsvForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 166);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FileNameTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.QuitButton);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.InvestigatorComboBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MinimumSize = new System.Drawing.Size(275, 200);
+            this.Name = "AddDeploymentCsvForm";
+            this.Text = "Add CSV Deployment Data";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button QuitButton;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.ComboBox InvestigatorComboBox;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox FileNameTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
