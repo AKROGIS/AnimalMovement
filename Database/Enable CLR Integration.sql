3838a1121d6ece14e8a871c904ec956c843fed72
Regan Sarwas (work) <regan_sarwas@nps.gov>
2013-06-13 13:46:02 -0800

    Rearrange some documentation files

--- Database/Enable CLR Integration.sql
+++
@@ -1,6 +0,0 @@
--- http://msdn.microsoft.com/en-us/library/w2kae45k(v=VS.90).aspx
--- http://msdn.microsoft.com/en-us/library/ms131048(v=SQL.100).aspx
-sp_configure 'clr enabled', 1
-GO
-RECONFIGURE
-GO
