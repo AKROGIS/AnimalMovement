25be977929cdb5ffe603c07b7dbf807795393215
Regan Sarwas <regan_sarwas@nps.gov>
2016-08-09 11:41:52 -0800

    Script to parse John Burch's VHF wolf data

---
+++ Toolbox/Build_VHF_Locations.py
@@ -0,0 +1,62 @@
+import csv
+import datetime
+
+# This parser is specific for a single input file as described here.
+project_id = 'Yuch_Wolf'
+species = 'Wolf'
+filename = r'YuchWolfVHFLocations.csv'
+file_id = '58310'  # Get this number from the database after the file above has been loaded.
+
+
+def make_datetime(date, time):
+    month, day, year = date.split('/')
+    hour, minute = 0, 0  # Assume Midnight if no time given
+    if time and time != '0' and time != '-9':
+        if len(time) == 3:
+            hour, minute = time[0], time[1:3]
+        else:
+            hour, minute = time[0:2], time[2:4]
+    return datetime.datetime(int(year), int(month), int(day), int(hour), int(minute))
+
+
+def make_sql():
+    header = 'FileId,LineNumber,ProjectID,AnimalId,Species,GroupName,Description,LocalFixDate,Location'
+    sql = "INSERT INTO [VHFLocations] (" + header + ") VALUES ({0})\n"
+    with open(filename, 'rb') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        with open(r'VHFLocations.sql', 'w') as sql_file:
+            csv_reader.next()  # throw away the header (two lines)
+            csv_reader.next()
+            line = 2
+            for row in csv_reader:
+                line += 1
+                date = make_datetime(row[0], row[12])
+                wolves = row[7:11]
+                lat, lon = row[39], row[40]
+                if lat and lon:
+                    try:
+                        lat = float(lat)
+                        lon = float(lon)
+                    except (ValueError, TypeError):
+                        print 'lat or lon is not a float at', line, lat, lon
+                    if lat and lon:
+                        for wolf in wolves:
+                            if wolf and wolf != '0':
+                                values = [file_id,
+                                          str(line),
+                                          "'" + project_id + "'",
+                                          "'" + wolf + "'",
+                                          "'" + species + "'",
+                                          "'" + row[5] + "'",
+                                          "'" + row[43].replace("'","''") + "'",
+                                          "'" + str(date) + "'",
+                                          build_geom(lat, lon)]
+                                values = ','.join(values)
+                                sql_file.write(sql.format(values))
+
+
+def build_geom(lat, lon):
+    return "geography::Point({0}, {1}, 4326)".format(lat, lon)
+
+
+make_sql()
