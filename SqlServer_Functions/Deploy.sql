1c36f748e49870c3953a88316c1f1da3b08c6dc9
Regan Sarwas <regan_sarwas@nps.gov>
2020-09-29 14:40:06 -0800

    Delete unused SQL deployment scripts

--- SqlServer_Functions/Deploy.sql
+++ SqlServer_Functions/Deploy.sql
@@ -1,5 +1,28 @@
 ﻿USE [Animal_Movement]
 GO
+/*
+-- It may be much simpler to just alter  the assembly if the function names/interfaces have not changed
+
+ALTER ASSEMBLY ComplexNumber 
+FROM 'E:\transfer\assemblies\SqlServer_Functions.dll'
+
+*/
+
+
+/*
+-- Need to remove all reference (in tables, views, etc) to the functions
+-- WARNING: The following is incomplete:
+
+IF COL_LENGTH('CollarFiles','Sha1Hash') IS NOT NULL --safely check if column exists
+BEGIN
+    alter table [Animal_Movement].[dbo].[collarfiles] drop column [Sha1Hash]
+END
+
+IF COL_LENGTH('CollarParameterFiles','Sha1Hash') IS NOT NULL --safely check if column exists
+BEGIN
+    alter table [Animal_Movement].[dbo].[CollarParameterFiles] drop column [Sha1Hash]
+END
+*/
 
 DROP FUNCTION [dbo].[LocalTime] 
 DROP FUNCTION [dbo].[UtcTime]
@@ -38,3 +61,11 @@ RETURNS [varbinary](8000)
 AS EXTERNAL NAME [SqlServer_Functions].[SqlServer_Functions.SimpleFunctions].[Sha1Hash];
 GO
 
+/*
+-- Need to recreate all reference (in tables, views, etc) to the functions
+-- WARNING: The following is incomplete:
+
+alter table [Animal_Movement].[dbo].[collarfiles] add [Sha1Hash] AS ([dbo].[Sha1Hash]([Contents])) PERSISTED
+alter table [Animal_Movement].[dbo].[CollarParameterFiles] add [Sha1Hash] AS ([dbo].[Sha1Hash]([Contents])) PERSISTED
+
+*/
